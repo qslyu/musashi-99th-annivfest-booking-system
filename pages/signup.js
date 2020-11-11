44c58e093ceedcff242c8898c04fcccba7be7022
aakawazu <aakawazu@protonmail.com>
2020-11-11 17:44:20 +0900

    /signupで入力値をバリデートするようにした

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import InitFirebase from '../utils/firebase/init'
-import { Heading, Box, Button, Form, FormField, TextInput } from 'grommet'
+import { Heading, Box, Form, FormField, TextInput, Text } from 'grommet'
+import ButtonLoader from '../components/buttonLoader'
 import Layout from '../components/layout'
+import { usernameRules, emailRules, passwordRules } from '../utils/grommet/rules'
 
 export default function SignUp() {
   const router = useRouter()
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState()
 
   function signUp(val) {
+    setLoading(true)
+    setError()
     InitFirebase()
     
     const displayname = val.displayname
@@ -22,7 +29,11 @@ export default function SignUp() {
           .catch(err => console.log(err))
       })
       .then(() => router.push('/emailsent'))
-      .catch(err => console.log(err))
+      .catch(err => {
+        setLoading(false)
+        setError(err.message)
+        console.log(err)
+      })
   }
 
   return (
@@ -30,29 +41,38 @@ export default function SignUp() {
       <Heading>新規登録</Heading>
       <Form
         onSubmit={({ value }) => {signUp(value)}}
+        validate="submit"
       >
         <FormField
           name="displayname"
           label="名前"
+          validate={usernameRules}
         >
           <TextInput name="displayname" />
         </FormField>
         <FormField
           name="email"
           label="メールアドレス"
+          validate={emailRules}
         >
           <TextInput name="email" />
         </FormField>
         <FormField
           name="password"
           label="パスワード"
+          validate={passwordRules}
         >
           <TextInput type="password" name="password" />
         </FormField>
         <Box direction="column" top="large">
-          <Button type="submit" primary label="登録" />
+          <ButtonLoader label="登録" labelLoad="送信中" isLoading={isLoading} />
         </Box>
-      </Form> 
+      </Form>
+      <Box pad="medium">
+        <Text color="status-error">
+          {error}
+        </Text>
+      </Box>
     </Layout>
   )
 }
