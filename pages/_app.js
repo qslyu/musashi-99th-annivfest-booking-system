b6ceb717c787d71a368e2dc9e700a14b7e831ef8
aakawazu <aakawazu@protonmail.com>
2020-11-14 20:09:01 +0900

    Appコンポーネントでfirebaseを初期化するようにした

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import { Grommet, grommet as grommetTheme } from 'grommet'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import initFirebase from '../utils/firebase/init'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    initFirebase()
+
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        const pathname = router.pathname
+
+        if(pathname == '/signup' || pathname == '/login') router.push('/')
+        if(!user.emailVerified) router.push('/emailsent')
+      }
+    })
+  })
+
   return (
     <Grommet theme={grommetTheme}>
       <Component {...pageProps} />
