67686e6007028941f5c5b702ae199928d1bcc634
aakawazu <aakawazu@protonmail.com>
2020-12-04 04:44:28 +0900

    rename

---
+++ utils/validateTimeID.js
@@ -0,0 +1,42 @@
+import { connectToDatabase } from './mongodb'
+import timeList from '../time_list.json'
+
+function isExists(timeID) {
+  const index = timeList.findIndex(timeInfo => timeInfo.id == timeID)
+  if(index == -1) {
+    throw new Error('time not found')
+  }
+  return index
+}
+
+export async function isReserved(userID, timeID) {
+  isExists(timeID)
+
+  const { db } = await connectToDatabase()
+
+  const reservedByUser = await db.collection('bookings')
+    .findOne({
+      user: userID,
+      time_id: timeID
+    })
+    .then(data => {
+      if(data) return data._id
+    })
+
+  return reservedByUser
+}
+
+export async function isFull(timeID) {
+  const index = isExists(timeID)
+  const limit = timeList[index].limit
+
+  const { db } = await connectToDatabase()
+
+  const reserved = await db.collection('bookings')
+    .find({
+      time_id: timeID
+    })
+    .count()
+
+  return reserved >= limit
+}
