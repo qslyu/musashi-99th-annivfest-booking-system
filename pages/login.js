c06c32ea61965ad19af238e0b06fedceb0e76dd2
aakawazu <aakawazu@protonmail.com>
2020-12-21 15:06:54 +0900

    予約番号で登録できるようにした

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import 'firebase/auth'
 import { Heading, Box, Form, FormField, TextInput, Text } from 'grommet'
 import ButtonLoader from '../components/buttonLoader'
 import Layout from '../components/layout'
-import { emailRules, passwordRules } from '../utils/grommet/rules'
+import { numberRule, passwordRule } from '../utils/grommet/rules'
 
 export default function Login() {
   const router = useRouter()
@@ -17,14 +17,20 @@ export default function Login() {
     setLoading(true)
     setError()
     
-    const email = val.email
+    const email = `${val.number}@634-annivfest.jp`
     const password = val.password
   
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(() => router.push('/'))
       .catch(err => {
         setLoading(false)
-        setError(err.message)
+
+        const errCode = err.code
+        if(errCode == 'auth/user-not-found' || errCode == 'auth/wrong-password') {
+          setError('予約番号もしくはパスワードが違います')
+        } else {
+          setError(err.message)
+        }
       })
   }
 
@@ -36,18 +42,24 @@ export default function Login() {
         validate="submit"
       >
         <FormField
-          name="email"
-          label="メールアドレス"
-          validate={emailRules}
+          name="number"
+          label="予約番号"
+          required
+          validate={numberRule}
         >
-          <TextInput name="email" />
+          <TextInput
+            name="number"
+          />
         </FormField>
         <FormField
           name="password"
           label="パスワード"
-          validate={passwordRules}
+          validate={passwordRule}
         >
-          <TextInput type="password" name="password" />
+          <TextInput
+            type="password"
+            name="password"
+          />
         </FormField>
         <Box direction="column" top="large">
           <ButtonLoader label="ログイン" labelLoad="送信中" isLoading={isLoading} />
